5a458d829dc115ee6f86a216d0e80009193dd0bc
Italo Guilherme de Meneses e Costa <italo.costa@mixalito.local>
2025-04-16 09:42:21 -0300

    ...

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -183,7 +183,7 @@ export function Cart() {
                 {new Intl.NumberFormat('pt-br', {
                   currency: 'BRL',
                   style: 'currency',
-                }).format(DELIVERY_PRICE)}
+                }).format(DELIVERY_PRICE * amountTags.length)}
               </span>
             </div>
 
